c084bd4de0110aed5ab779d4843d6b967957e94d
azix-khan <azixkhan.55@gmail.com>
2024-08-28 12:46:15 +0500

    cards added and removed unused code

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:credit_cards/credit_cards_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ,
+      home: CreditCardConceptPage(),
     );
   }
 }
